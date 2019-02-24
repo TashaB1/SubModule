1e334b8ca3a866ac5ca4b26e6f7f1c9601e1cf81
TashaB1 <tasha2k7@mail.ru>
2019-02-25 01:22:49 +0300

    add button (onclick show Toast)

--- app/src/main/java/com/tashad16a/gmail/submodule/MainActivity.kt
+++ app/src/main/java/com/tashad16a/gmail/submodule/MainActivity.kt
@@ -2,11 +2,25 @@ package com.tashad16a.gmail.submodule
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showToastBye(view: View){
+        //создаём и отображаем текстовое уведомление
+        val toast = Toast.makeText(
+            this,
+            "Goodbye world!",
+            Toast.LENGTH_LONG  //(По умолчанию) показывает текстовое уведомление на короткий промежуток времени
+        )
+        toast.setGravity(Gravity.CENTER, 0, 0);   //позиция на экране
+        toast.show()
     }
 }
